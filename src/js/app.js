c8e29d1b16d3f64669124c8c573d8e06a6cc591d
Дмитрий <melkdim@list.ru>
2022-04-08 13:29:37 +0500

    add corrections

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functionsWebP.js";
 import { selectCustom } from "./selectCustom.js";
+import { burger } from "./burger.js";
 
 flsFunctions.isWeb();
 selectCustom();
+burger();
 
 // import Swiper, { Navigation, Pagination} from "swiper";
 // const swiper = new Swiper();
