152e1ac0995b8f401e4baac2de27d18c45a0fbaf
Dmitry <melkdim@list.ru>
2022-08-26 17:04:28 +0500

    add corrections

--- README.md
+++
@@ -1,2 +0,0 @@
-# SitDownPls
-internet-shop
--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export let configFTP = {
-  host: "", //Адрес FTP сервера
-  user: "",
-  password: "",
-  parallel: 5, //кол-во одновременных потоков
-};
